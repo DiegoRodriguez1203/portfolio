ee573660a5ccd85656150dad9acae8b9e3adfbac
DiegoRodriguez1203 <diegorodriguez1203@gmail.com>
2023-01-16 12:44:41 -0500

    proyect component made

--- src/components/Proyect.jsx
+++ src/components/Proyect.jsx
@@ -1,10 +1,21 @@
 import '../style/proyect.css'
+import github from '../img/proyects/icons/github.svg'
+import rocket from '../img/proyects/icons/rocket.svg'
+import sahury from '../img/proyects/photos/sahury.png'
 
 export const Proyect = () => {
     return(
         <div className="proyect">
             <div className='proyect-img'>
-            <img src="" alt="" />
+            <img src={sahury} alt="" />
+            </div>
+            <div className='proyect-text'>
+            <h1>Sahury Spa</h1>
+            <p>A cart market made for Sahury-spa, a small beauty company who wanted to have a reservation and catalogue web app.</p>
+            <div className="proyect-logos">
+                <img src={github} alt="" />
+                <img src={rocket} alt="" />
+            </div>
             </div>
         </div>
     )
